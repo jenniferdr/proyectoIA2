823e6c904d427f85eaeaba6fc6d0f06f7b93c18f
José Alberto Goncalves Da Silva <gml.josea2@gmail.com>
2012-11-03 18:30:34 -0430

    Lo que llevo de alphabeta, don't touch

--- src/alphabeta.cc
+++ src/alphabeta.cc
@@ -1,40 +1,72 @@
 #include "algoritmos.hh"
 #include <iostream>
 
+#include <tr1/unordered_map>
+
 using namespace std;
 
+struct stored_info_t {
+  // [information to be stored in hash table]
+  int minimax;
+  int high;
+  int low;
+  stored_info_t() { } // need at least one ctor without arguments
+};
+
+struct hash_function_t : public tr1::hash<state_t> {
+  size_t operator()(const state_t &state) const {
+    return state.hash();
+  }
+};
+
+class hash_table_t : public tr1::unordered_map<state_t, stored_info_t, hash_function_t> {
+};
+
 int expanded_nodes = 0;
+
+hash_table_t states;
 
 int alphabeta_othello(state_t node, int alpha, int beta, bool color) {
 
   expanded_nodes++;
-  cout << expanded_nodes << endl;
 
   if (node.terminal()) {
-    //    cout << node << endl;
     return node.value();
   }
 
-  if (color) { // color = true representa las negras, el cual es MAX
+  bool pass = true;
 
+  if (color) { // color = true representa las negras, el cual es MAX
     for( int pos = 0; pos < DIM; ++pos ) {
       if (node.is_black_move(pos)) {
+	pass = false;
 	alpha = MAX(alpha, alphabeta_othello(node.move(color, pos),
 					     alpha, beta, !color));
 	if (beta <= alpha) break;
       }
     }
+
+    if (pass) {
+      alpha = MAX(alpha, alphabeta_othello(node, alpha, beta, !color));
+    }
+
     return alpha;
 
   } else {
 
     for( int pos = 0; pos < DIM; ++pos ) {
       if( (node.is_white_move(pos)) ) {
+	pass = false;
 	beta = MIN(beta, alphabeta_othello(node.move(color, pos),
 					   alpha, beta, !color));
 	if (beta <= alpha) break;
       }
     }
+
+    if (pass) {
+      beta = MIN(beta, alphabeta_othello(node, alpha, beta, !color));
+    }
+
     return beta;
 
   }
--- src/main.cc
+++ src/main.cc
@@ -14,23 +14,33 @@ using namespace std;
 int main(int argc, const char **argv) {
     state_t state;
 
-    int bla = alphabeta_othello(state, INT_MIN, INT_MAX, true);
-    cout << bla << endl;
+    state_t pv_states[32];
 
-    /*    cout << "Principal variation:" << endl;
+    //alphabeta_othello(state, INT_MIN, INT_MAX, true);
+
+    cout << "Principal variation:" << endl;
     for( int i = 0; PV[i] != -1; ++i ) {
         bool player = i % 2 == 0; // black moves first!
         int pos = PV[i];
-        cout << state;
-        cout << (player ? "Black" : "White")
+        //cout << state;
+	pv_states[i] = state;
+	/*        cout << (player ? "Black" : "White")
              << " moves at pos = " << pos << (pos == 36 ? " (pass)" : "")
-             << endl;
+             << endl;*/
         state = state.move(player, pos);
-        cout << "Board after " << i+1 << (i == 0 ? " ply:" : " plies:") << endl;
+	//        cout << "Board after " << i+1 << (i == 0 ? " ply:" : " plies:") << endl;
     }
     cout << state;
     cout << "Value of the game = " << state.value() << endl;
     cout << "#bits per state = " << sizeof(state) * 8 << endl;
+
+    for ( int i = 32; i >= 0; --i) {
+      if (i % 2 != 0) continue;
+      cout << "i: " << i << endl;
+      cout << "pv: " << PV[i] << endl;
+      cout << pv_states[i];
+      cout << "minmax val: " << alphabeta_othello(pv_states[i], INT_MIN, INT_MAX, true) << endl;
+    }
 
     if( argc > 1 ) {
         int n = atoi(argv[1]);
@@ -45,5 +55,5 @@ int main(int argc, const char **argv) {
         cout << endl << state;
     }
 
-    return 0;*/
+    return 0;
 }
