822392c7d98ad47e28c046f2b431db2107658b5e
Javier <javier@ubuntu.(none)>
2012-11-05 18:37:09 -0430

    modificacion de minmax

--- src/minmax.cc
+++ src/minmax.cc
@@ -8,10 +8,10 @@ int minmax_othello(state_t node, bool color) {
   int alpha = INT_MAX;
 
   for( int pos = 0; pos < DIM; ++pos ) {
-    if( (color && node.is_black_move(pos))
-	|| (!color && node.is_white_move(pos)) ) {
+    //if( (color && node.is_black_move(pos))
+	//|| (!color && node.is_white_move(pos)) ) {
       alpha = MAX(INT_MAX, -minmax_othello(node.move(color, pos), color));
-    }
+    //}
   }
 
   return alpha;
