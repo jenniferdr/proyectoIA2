a8241f8763c20961153bc10e7f8ef79da5cee57e
jenniferdr <jenniferdosreis@hotmail.com>
2012-11-05 12:15:17 -0430

    FUNCIONNNAAAA YAAAAYYY :D

--- src/negascout.cc
+++ src/negascout.cc
@@ -1,6 +1,9 @@
 #include "algoritmos.hh"
+extern long long int expanded_nodes;
 
 int negascout(state_t node,int alpha,int beta,bool color){
+
+  expanded_nodes++;
 
   if(node.terminal()){
     return node.value();
@@ -8,18 +11,33 @@ int negascout(state_t node,int alpha,int beta,bool color){
 
   //int m= INT_MIN; // Cota inferior 
   int b= beta;
+  bool pass=true;
 
   if(color){ // Generar jugadas del turno black
 
-    for(int poss=0; poss <= DIM; ++poss){
-      if(!node.is_black_move(poss)) continue;
-      int a= -negascout(node.move(color,poss),-b,-alpha,!color);
+    for(int pos=0; pos < DIM; ++pos){
+      if(!node.is_black_move(pos)) continue;
+      int a= -negascout(node.move(color,pos),-b,-alpha,!color);
+      
       if( a > alpha ) alpha=a;
       if( alpha >= beta ) return alpha;
 
       if( alpha >= b ){
 	// Si fallo la ventana nula hacer nueva busqueda completa
-	alpha=-negascout(node.move(color,poss),-beta,-alpha,!color);
+	alpha=-negascout(node.move(color,pos),-beta,-alpha,!color);
+	if( alpha >= beta ) return alpha;
+      }
+      b= alpha+1; // Crea la nueva ventana nula
+      pass=false;
+    }
+    if(pass){
+      int a= -negascout(node,-b,-alpha,!color);
+      if( a > alpha ) alpha=a;
+      if( alpha >= beta ) return alpha;
+
+      if( alpha >= b ){
+	// Si fallo la ventana nula hacer nueva busqueda completa
+	alpha=-negascout(node,-beta,-alpha,!color);
 	if( alpha >= beta ) return alpha;
       }
       b= alpha+1; // Crea la nueva ventana nula
@@ -28,15 +46,29 @@ int negascout(state_t node,int alpha,int beta,bool color){
 
   }else{ // Generar jugadas del turno white
 
-    for(int poss=0; poss <= DIM; ++poss){
-      if(!node.is_white_move(poss)) continue;
-      int a= -negascout(node.move(color,poss),-b,-alpha,!color);
+     for(int pos=0; pos < DIM; ++pos){
+      if(!node.is_white_move(pos)) continue;
+      int a= -negascout(node.move(color,pos),-b,-alpha,!color);
+      
       if( a > alpha ) alpha=a;
       if( alpha >= beta ) return alpha;
 
       if( alpha >= b ){
 	// Si fallo la ventana nula hacer nueva busqueda completa
-	alpha=-negascout(node.move(color,poss),-beta,-alpha,!color);
+	alpha=-negascout(node.move(color,pos),-beta,-alpha,!color);
+	if( alpha >= beta ) return alpha;
+      }
+      b= alpha+1; // Crea la nueva ventana nula
+      pass=false;
+    }
+    if(pass){
+      int a= -negascout(node,-b,-alpha,!color);
+      if( a > alpha ) alpha=a;
+      if( alpha >= beta ) return alpha;
+
+      if( alpha >= b ){
+	// Si fallo la ventana nula hacer nueva busqueda completa
+	alpha=-negascout(node,-beta,-alpha,!color);
 	if( alpha >= beta ) return alpha;
       }
       b= alpha+1; // Crea la nueva ventana nula
